b1ecf68ddf8f59ab09e3d85e4caee884675cabd0
Bhaskar Tripathi <35177508+bhaskatripathi@users.noreply.github.com>
2023-03-25 04:07:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 import openai
 import streamlit as st
 import pandas as pd
-
-import pandas as pd
-
-def response_to_dataframe(response):
-    rows = response.split("\n")
-    data = []
-
-    for row in rows:
-        if not row:
-            continue
-
-        columns = row.split(",")
-        data.append([col.strip() for col in columns])
-
-    df = pd.DataFrame(data, columns=["Journal", "Impact Factor", "Indexed In", "Acceptance Rate", "Review Speed", "URL"])
-    return df
-
+import re
 
 def chat_gpt_request(api_key, messages):
     openai.api_key = api_key
@@ -41,32 +25,22 @@ def journal_finder(api_key, title, abstract, ssci, scie, esci, keywords):
     indexed_str = ", ".join(indexed_in)
     publishers = "Sciencedirect, MDPI, IEEE, Wiley, Peerj, Emerald, PLOS"
 
-    prompt = f"Find the top 15 best-matching journals for the following paper that are indexed in {indexed_str} and are published by {publishers}. Include information on Impact Factor, Acceptance Rate, Review Speed, and URLs.\n\nTitle: {title}\n\nAbstract: {abstract}\n\nKeywords: {keywords}\n\n"
+    prompt = f"Find the top 10 best-matching journals for the following paper that are indexed in {indexed_str} and are published by {publishers}. Include information on Impact Factor, Acceptance Rate, Review Speed, and URLs.\n\nTitle: {title}\n\nAbstract: {abstract}\n\nKeywords: {keywords}\n\n"
 
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=2000,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     )
 
     return response.choices[0].text.strip()
+
+
+
 st.title("Journal Finder")
-
-def response_to_html_table(response):
-    rows = response.split("\n")
-    html_table = "<table border='1'>"
-    for row in rows:
-        html_table += "<tr>"
-        columns = row.split(",")
-        for column in columns:
-            html_table += f"<td>{column.strip()}</td>"
-        html_table += "</tr>"
-    html_table += "</table>"
-    return html_table
-
 
 with st.sidebar:
     api_key = st.text_input("API Key (masked)", type="password")
@@ -80,25 +54,12 @@ with st.sidebar:
 if st.button("Find Journals"):
     if api_key and title and abstract and keywords:
         result = journal_finder(api_key, title, abstract, ssci, scie, esci, keywords)
-        
-        # Convert the response to an HTML table
-        html_table = response_to_html_table(result)
-        
-        # Display the HTML table in Streamlit
-        st.markdown(html_table, unsafe_allow_html=True)
 
-        # Convert the response to a DataFrame
-        df = response_to_dataframe(result)
-
-        # Export to CSV button
-        if st.button("Export to CSV"):
-            filename = "journals.csv"
-            df.to_csv(filename, index=False)
-            st.success(f"File exported as {filename}")
+        # Print the result
+        st.write("Result:")
+        st.write(result)
     else:
         st.error("Please fill in all required fields.")
-
-
 
 
 
