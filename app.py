8c4a613e75c8d2d8ae94935fa71c854f27c3bcfa
Bhaskar Tripathi <35177508+bhaskatripathi@users.noreply.github.com>
2023-03-25 03:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,41 +60,11 @@ if st.button("Find Journals"):
         journal_list = re.split(r"\d{1,2}\.\s", result)[1:]
 
         for item in journal_list:
-            name_match = re.search(r"^(.*)\s*\(", item)
-            if name_match:
-                name = name_match.group(1)
-            else:
-                continue
+            journal_data.append(item.split("\n"))
 
-            impact_factor_match = re.search(r"Impact Factor:\s*([\d.]+)", item)
-            if impact_factor_match:
-                impact_factor = impact_factor_match.group(1)
-            else:
-                impact_factor = ""
+        df = pd.DataFrame(journal_data, columns=["Journal", "Impact Factor", "Indexed", "Acceptance Rate", "Review Speed", "Link"])
 
-            indexed_match = re.search(r"Indexed:\s*([\w\s]+)", item)
-            if indexed_match:
-                indexed = indexed_match.group(1)
-            else:
-                indexed = ""
-
-            acceptance_rate_match = re.search(r"Acceptance Rate:\s*([A-Za-z\s]+)", item)
-            if acceptance_rate_match:
-                acceptance_rate = acceptance_rate_match.group(1)
-            else:
-                acceptance_rate = "Not publicly available"
-
-            review_speed_match = re.search(r"Review Speed:\s*(.*?)\s*\n", item, re.MULTILINE)
-            if review_speed_match:
-                review_speed = review_speed_match.group(1)
-            else:
-                review_speed = ""
-
-            link_match = re.search(r"Link:\s*(\S+)", item)
-            if link_match:
-                link = link_match.group(1)
-            else:
-                link = ""
-
-            journal_data.append([name, impact_factor, indexed, acceptance_rate, review_speed, link])
+        st.write(df)
+    else:
+        st.error("Please fill in all required fields.")
 
